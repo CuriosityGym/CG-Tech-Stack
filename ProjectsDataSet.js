dfd1d80347845bff42338da3a59d8d984d5aaa6f
siddheshmurudkar <32839650+siddheshmurudkar@users.noreply.github.com>
2017-11-23 17:50:28 +0530

    Update ProjectsDataSet.js

--- ProjectsDataSet.js
+++ ProjectsDataSet.js
@@ -84,4 +84,207 @@ addComponent("Quadruped", "Arduino","5" ,"Yellow");
 addComponent("Quadruped", "Infrared","4" ,"orange");
 addComponent("Quadruped", "Servo","4" ,"GreenYellow");
 addComponent("Quadruped", "3D Printing","9" ,"Violet");
+
+//MeArm
+addComponent("MeArm", "Arduino","5" ,"Yellow");
+addComponent("MeArm", "Orientation","9" ,"orange");
+addComponent("MeArm", "Bend/Flex","9" ,"orange");
+addComponent("MeArm", "Servo","4" ,"GreenYellow");
+addComponent("MeArm", "3D Printing","9" ,"Violet");
+addComponent("MeArm", "BT/BLE","10" ,"green");
+	
+//Line Following Robot		
+addComponent("Line Following Robot", "Arduino","5" ,"Yellow");
+addComponent("Line Following Robot", "Ultrasonic","9" ,"orange");
+addComponent("Line Following Robot", "Infrared","9" ,"orange");
+addComponent("Line Following Robot", "DC Motor","4" ,"GreenYellow");
+addComponent("Line Following Robot", "3D Printing","9" ,"Violet");
+
+//Twitter Lamp
+addComponent("Twitter Lamp", "ESP8266","10" ,"Yellow");
+addComponent("Twitter Lamp", "Wifi","9" ,"Green");
+addComponent("Twitter Lamp", "LED/WS2812","8" ,"GreenYellow ");
+addComponent("Twitter Lamp", "3D Printing","9" ,"Violet");
+
+
+//IOT Mote
+addComponent("IOT Mote", "ESP8266","10" ,"Yellow");
+addComponent("IOT Mote", "Wifi","9" ,"Green");
+addComponent("IOT Mote", "Temperature","4","orange");
+addComponent("IOT Mote", "Humidity","6","orange");
+addComponent("IOT Mote", "MQTT","9" ,"blue");
+addComponent("IOT Mote", "Thingspeak","12","cyan");
+
+//Mood Lamp
+addComponent("Mood Lamp", "Wifi","20","green");
+addComponent("Mood Lamp", "ESP8266","10","yellow");
+addComponent("Mood Lamp", "LED/WS2812","8" ,"GreenYellow ");
+addComponent("Mood Lamp", "3D Printing","9" ,"Violet");
+//Balance BOT
+addComponent("Balance BOT", "PID", "10");
+addComponent("Balance BOT", "IMU", "10");
+addComponent("Balance BOT", "Arduino","3","yellow");
+addComponent("Balance BOT", "Orientation","9" ,"orange");
+addComponent("Balance BOT", "DC Motor","10" ,"GreenYellow ");
+addComponent("Balance BOT", "3D Printing","9" ,"Violet");
+
+//Drones
+addComponent("Drones", "IMU", "10");
+addComponent("Drones", "Wifi","20","green");
+addComponent("Drones", "BT/BLE","10","green");
+addComponent("Drones", "GPS/GPRS","6","yellow");
+addComponent("Drones", "Orientation","9" ,"orange");
+addComponent("Drones", "3D Printing","9" ,"Violet");
+//CG Wand
+addComponent("CG Wand", "IMU", "10");
+addComponent("CG Wand", "BT/BLE","10","green");
+addComponent("CG Wand", "Arduino","3","yellow");
+
+//Maker Wall
+addComponent("Maker Wall", "Arduino","3","yellow");
+
+//Smart Box/Supply Chain Transportation System
+addComponent("Smart Box/Supply Chain Transportation System", "Arduino","3","yellow");
+addComponent("Smart Box/Supply Chain Transportation System", "NRF51822","8","yellow");
+addComponent("Smart Box/Supply Chain Transportation System", "ESP8266","9","red");
+addComponent("Smart Box/Supply Chain Transportation System", "DC Motor","10" ,"GreenYellow ");
+
+//Air Column
+addComponent("Air Column", "Arduino","20","magenta");
+addComponent("Air Column", "RS-232","12","purple");
+addComponent("Air Column", "USB","8","purple");
+addComponent("Air Column", "Arduino","3","yellow");
+addComponent("Air Column", "NRF51822","8","yellow");
+addComponent("Air Column", "Pressure","3" ,"orange");
+addComponent("Air Column", "Fan","9" ,"GreenYellow ");
+addComponent("Air Column", "Displays","3", "GreenYellow ");
+addComponent("Air Column", "Analog","10" ,"blue");
+addComponent("Air Column", "Serial","8" ,"blue");
+
+//Automatic Object Sorter with transport, using colour recognition
+addComponent("Automatic Object Sorter with transport", "Arduino","20","magenta");
+addComponent("Automatic Object Sorter with transport", "RS-232","12","purple");
+addComponent("Automatic Object Sorter with transport", "USB","8","purple");
+addComponent("Automatic Object Sorter with transport", "Color","4" ,"orange");
+addComponent("Automatic Object Sorter with transport", "Infrared","10" ,"orange");
+addComponent("Automatic Object Sorter with transport", "DC Motor","10" ,"GreenYellow ");
+addComponent("Automatic Object Sorter with transport", "Servo","4","GreenYellow ");
+
+//Ping Pong
+addComponent("Ping Pong", "Arduino","20","magenta");
+addComponent("Ping Pong", "Wifi","20","green");
+addComponent("Ping Pong", "ESP8266","10","yellow");
+addComponent("Ping Pong", "ESP8266","9","red");
+addComponent("Ping Pong", "Knock","8" ,"orange");
+
+//Uber One Button Booking
+addComponent("Uber One Button Booking", "Arduino","20","magenta");
+addComponent("Uber One Button Booking", "Wifi","20","green");
+addComponent("Uber One Button Booking", "ESP8266","10","yellow");
+addComponent("Uber One Button Booking", "ESP8266","9","red");
+addComponent("Uber One Button Booking", "LED/WS2812","8" ,"GreenYellow ");
+addComponent("Uber One Button Booking", "3D Printing","9" ,"Violet");
+//Pill Reminder
+addComponent("Pill Reminder", "Arduino","20","magenta");
+addComponent("Pill Reminder", "Wifi","20","green");
+addComponent("Pill Reminder", "ESP8266","10","yellow");
+addComponent("Pill Reminder", "ESP8266","9","red");
+addComponent("Pill Reminder", "LED/WS2812","8" ,"GreenYellow ");
+addComponent("Pill Reminder", "Blynk","10","cyan");
+
+
+//Flight Alert
+addComponent("Flight Alert", "Arduino","20","magenta");
+addComponent("Flight Alert", "Wifi","20","green");
+addComponent("Flight Alert", "ESP8266","10","yellow");
+addComponent("Flight Alert", "ESP8266","9","red");
+addComponent("Flight Alert", "DC Motor","10" ,"GreenYellow ");
+
+//Diaper Monitoring
+addComponent("Diaper Monitoring", "Arduino","20","magenta");
+addComponent("Diaper Monitoring", "Wifi","20","green");
+addComponent("Diaper Monitoring", "ESP8266","10","yellow");
+addComponent("Diaper Monitoring", "ESP8266","9","red");
+addComponent("Diaper Monitoring", "LED/WS2812","8" ,"GreenYellow ");
+addComponent("Diaper Monitoring", "IFTTT","10","cyan");
+
+//Spool Weight Measurement
+addComponent("Spool Weight Measurement", "Arduino","20","magenta");
+addComponent("Spool Weight Measurement", "Wifi","20","green");
+addComponent("Spool Weight Measurement", "ESP8266","10","yellow");
+addComponent("Spool Weight Measurement", "ESP8266","9","red");
+addComponent("Spool Weight Measurement", "Thingspeak","12","cyan");
+addComponent("Spool Weight Measurement", "3D Printing","9" ,"Violet");
+//XY Image Plotter
+addComponent("XY Image Plotter", "Arduino","20","magenta");
+addComponent("XY Image Plotter", "Arduino","3","yellow");
+addComponent("XY Image Plotter", "Displays","3", "GreenYellow ");
+addComponent("XY Image Plotter", "Servo","4","GreenYellow ");
+addComponent("XY Image Plotter", "DC Motor","10" ,"GreenYellow ");
+addComponent("XY Image Plotter", "3D Printing","9" ,"Violet");
+//Filament Cutter
+addComponent("Filament Cutter", "Arduino","20","magenta");
+addComponent("Filament Cutter", "Arduino","3","yellow");
+addComponent("Filament Cutter", "DC Motor","10" ,"GreenYellow ");
+addComponent("Filament Cutter", "3D Printing","9" ,"Violet");
+//OTTO Dancing Bot
+addComponent("OTTO Dancing Bot", "Servo","4","GreenYellow ");
+addComponent("OTTO Dancing Bot", "Ultrasonic","12" ,"orange");
+addComponent("OTTO Dancing Bot", "Arduino","3","yellow");
+addComponent("OTTO Dancing Bot", "Arduino","20","magenta");
+addComponent("OTTO Dancing Bot", "3D Printing","9" ,"Violet");
+//Camera Eye Shutter
+addComponent("Camera Eye Shutter", "Arduino","20","magenta");
+addComponent("Camera Eye Shutter", "Arduino","3","yellow");
+addComponent("Camera Eye Shutter", "Light","4" ,"orange");
+addComponent("Camera Eye Shutter", "Servo","4","GreenYellow ");
+addComponent("Camera Eye Shutter", "3D Printing","9" ,"Violet");
+//CG Robotics Bot
+addComponent("CG Robotics Bot", "Servo","4","GreenYellow ");
+addComponent("CG Robotics Bot", "Ultrasonic","12" ,"orange");
+addComponent("CG Robotics Bot", "Arduino","3","yellow");
+addComponent("CG Robotics Bot", "Arduino","20","magenta");
+addComponent("CG Robotics Bot", "Color","4" ,"orange");
+addComponent("CG Robotics Bot", "Infrared","10" ,"orange");
+addComponent("CG Robotics Bot", "Servo","4","GreenYellow ");
+addComponent("CG Robotics Bot", "LED/WS2812","8" ,"GreenYellow ");
+addComponent("CG Robotics Bot", "DC Motor","10" ,"GreenYellow ");
+addComponent("CG Robotics Bot", "3D Printing","9" ,"Violet");
+//CG Temp/Hum Mote
+addComponent("CG Temp/Hum Mote", "Arduino","20","magenta");
+addComponent("CG Temp/Hum Mote", "Wifi","20","green");
+addComponent("CG Temp/Hum Mote", "ESP8266","10","yellow");
+addComponent("CG Temp/Hum Mote", "ESP8266","9","red");
+addComponent("CG Temp/Hum Mote", "IFTTT","10","cyan");
+addComponent("CG Temp/Hum Mote", "Thingspeak","12","cyan");
+addComponent("CG Temp/Hum Mote", "Blynk","10","cyan");
+addComponent("CG Temp/Hum Mote", "Temperature","4","orange");
+addComponent("CG Temp/Hum Mote", "Humidity","6","orange");
+
+//Bicycle Generator
+addComponent("Bicycle Generator", "Arduino","20","magenta");
+addComponent("Bicycle Generator", "Wifi","20","green");
+addComponent("Bicycle Generator", "ESP8266","10","yellow");
+addComponent("Bicycle Generator", "ESP8266","9","red");
+addComponent("Bicycle Generator", "LED/WS2812","8" ,"GreenYellow ");
+addComponent("Bicycle Generator", "DC Motor","10" ,"GreenYellow ");
+addComponent("Bicycle Generator", "Hall","9" ,"orange");
+addComponent("Bicycle Generator", "3D Printing","9" ,"Violet");
+//Magic Mirror
+addComponent("Magic Mirror", "Raspbian","14","magenta");
+addComponent("Magic Mirror", "Wifi","20","green");
+addComponent("Magic Mirror", "RaspberryPi","4","yellow");
+
+//Solar Tracker
+addComponent("Solar Tracker", "Servo","4","GreenYellow ");
+addComponent("Solar Tracker", "Light","4" ,"orange");
+addComponent("Solar Tracker", "Arduino","3","yellow");
+addComponent("Solar Tracker", "Arduino","20","magenta");
+addComponent("Solar Tracker", "3D Printing","9" ,"Violet");
+//Alexa Voice Controlled Switch
+addComponent("Alexa Voice Controlled Switch", "Voice/TTS", "10");
+addComponent("Alexa Voice Controlled Switch", "Wifi","20","green");
+addComponent("Alexa Voice Controlled Switch", "ESP8266","10","yellow");
+addComponent("Alexa Voice Controlled Switch", "Relay","6","GreenYellow ");
+
 	
