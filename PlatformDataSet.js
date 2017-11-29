cfb9d01c159b4d2c186321d436738a04c17f783d
siddheshmurudkar <32839650+siddheshmurudkar@users.noreply.github.com>
2017-11-29 17:24:39 +0530

    Update PlatformDataSet.js

--- PlatformDataSet.js
+++ PlatformDataSet.js
@@ -8,88 +8,88 @@ addComponent("Motion Control", "IMU", "10", "white", "Inertial Measurement Unit"
 
 
 
-addComponent("Open Source", "OpenCV","10","magenta");
-addComponent("Open Source", "OSHWA","12","magenta");
-addComponent("Open Source", "Raspbian","14","magenta");
-addComponent("Open Source", "Scratch","8","magenta");
-addComponent("Open Source", "Arduino","20","magenta");
-addComponent("Open Source", "mBED","4","magenta");
-addComponent("Open Source", "Thingspeak","10","magenta");
-addComponent("Open Source", "IFTTT","10","magenta");
+addComponent("Open Source", "OpenCV","10","magenta","Open Source Computer Vision is a library of programming functions mainly aimed at real-time computer vision");
+addComponent("Open Source", "OSHWA","8","magenta","The Open Source Hardware Association (OSHWA) is a non-profit organization that advocates for open-source hardware.It aims to act as a hub of open source hardware activity of all types while actively cooperating with other initiatives such as the TAPR Open Hardware License, open-source development groups at CERN, and the Open Source Initiative (OSI)");
+addComponent("Open Source", "Raspbian","14","magenta","Raspbian is the raspberry pi’s official supported operating system");
+addComponent("Open Source", "Scratch","8","magenta"," free programming language and online community where you can create your own interactive stories, games, and animations");
+addComponent("Open Source", "Arduino","20","magenta","Open-source electronic prototyping platform enabling users to create interactive electronic objects");
+addComponent("Open Source", "mBED","4","magenta","The Arm Mbed IoT Device Platform provides the operating system, cloud services, tools and developer ecosystem to make the creation and deployment of commercial, standards-based IoT solutions possible at scale");
+addComponent("Open Source", "Thingspeak","12","magenta","IoT platform that lets you collect and store sensor data in the cloud and develop IoT applications");
+addComponent("Open Source", "IFTTT","12","magenta","IFTTT (if this, then that) is the easy, free way to get your apps and devices working together.");
 
 
-addComponent("API/WebServices", "IFTTT","10","cyan");
-addComponent("API/WebServices", "Thingspeak","12","cyan");
-addComponent("API/WebServices", "Bebottle","14","cyan");
-addComponent("API/WebServices", "CG Router","8","cyan");
-addComponent("API/WebServices", "Grove","20","cyan");
-addComponent("API/WebServices", "Exosite","4","cyan");
-addComponent("API/WebServices", "Cayenne","10","cyan");
-addComponent("API/WebServices", "Blynk","10","cyan");
+addComponent("API/WebServices", "IFTTT","12","cyan","IFTTT (if this, then that) is the easy, free way to get your apps and devices working together.");
+addComponent("API/WebServices", "Thingspeak","12","cyan","IoT platform that lets you collect and store sensor data in the cloud and develop IoT applications");
+addComponent("API/WebServices", "Bebottle","8","cyan");
+addComponent("API/WebServices", "CG Router","14","cyan");
+addComponent("API/WebServices", "Grove","10","cyan","Grove offers three API services: Simple POST for posting basic service messages to channels, HTTP API for building applications on top of Grove and pulling a variety of usage data, and Live API for requesting real-time updates via long polling.");
+addComponent("API/WebServices", "Exosite","4","cyan"," Internet of Things (IoT) platform that makes it easy to build connected products, solutions, and businesses");
+addComponent("API/WebServices", "Cayenne","10","cyan","The world's first drag-and-drop IoT project builder");
+addComponent("API/WebServices", "Blynk","20","cyan","It's an end-to-end solution which saves you time and resources when building applications for connected products and services.");
 
 
-addComponent("Comms-Wired", "Ethernet","10","purple");
-addComponent("Comms-Wired", "RS-232","12","purple");
-addComponent("Comms-Wired", "PoE","14","purple");
-addComponent("Comms-Wired", "USB","8","purple");
-
-
-
-addComponent("Comms-Wireless", "Wifi","20","green");
-addComponent("Comms-Wireless", "Zigbee","4","green");
-addComponent("Comms-Wireless", "ISM","10","green");
-addComponent("Comms-Wireless", "BT/BLE","10","green");
+addComponent("Comms-Wired", "Ethernet","10","purple"," network protocol that controls how data is transmitted over a LAN");
+addComponent("Comms-Wired", "RS-232","12","purple","RS-232 is a standard communication protocol for linking computer and its peripheral devices to allow serial data exchange");
+addComponent("Comms-Wired", "PoE","14","purple","Power over Ethernet or PoE describes any of several standard or ad-hoc systems which pass electric power along with data on twisted pair Ethernet cabling");
+addComponent("Comms-Wired", "USB","8","purple","A Universal Serial Bus (USB) is a common interface that enables communication between devices and a host controller such as a personal computer (PC)");
 
 
 
-addComponent("Boards", "Arduino","3","yellow");
-addComponent("Boards", "RaspberryPi","4","yellow");
-addComponent("Boards", "GPS/GPRS","6","yellow");
-addComponent("Boards", "Cortex M0/M3","10","yellow");
-addComponent("Boards", "NRF51822","8","yellow");
-addComponent("Boards", "ESP8266","10","yellow");
-addComponent("Boards", "ESP32","9","yellow");
+addComponent("Comms-Wireless", "Wifi","20","green","technology for wireless local area networking with devices based on the IEEE 802.11 standards");
+addComponent("Comms-Wireless", "Zigbee","8","green","low-cost and low-powered mesh network widely deployed for controlling and monitoring applications where it covers 10-100 meters within the range");
+addComponent("Comms-Wireless", "ISM","8","green","The industrial, scientific and medical (ISM) radio bands are radio bands (portions of the radio spectrum) reserved internationally for the use of radio frequency (RF) energy for industrial, scientific and medical purposes");
+addComponent("Comms-Wireless", "BT/BLE","14","green"," wireless technology standard for exchanging data over short distances from fixed and mobile devices");
 
-addComponent("Drivers", "Arduino","3", "red");
-addComponent("Drivers", "Linkit","4","red");
-addComponent("Drivers", "RaspberrPi","6","red");
+
+
+addComponent("Boards", "Arduino","10","yellow","Open-source electronic prototyping platform enabling users to create interactive electronic objects");
+addComponent("Boards", "RaspberryPi","6","yellow","tiny and affordable computer that you can use to learn programming through fun, practical projects");
+addComponent("Boards", "GPS/GPRS","6","yellow","board that combines GSM/GPRS and GPS into a single device");
+addComponent("Boards", "Cortex M0/M3","6","yellow","Smallest and Lowest Power Cortex Processors Optimized for deterministic real-time embedded processing and microcontroller applications");
+addComponent("Boards", "NRF51822","6","yellow","powerful, highly flexible multiprotocol SoC ideally suited for Bluetooth® low energy and 2.4GHz ultra low-power wireless applications");
+addComponent("Boards", "ESP8266","10","yellow","a low-cost Wi-Fi chip with full TCP/IP stack and MCU (microcontroller unit) capability ");
+addComponent("Boards", "ESP32","8","yellow","low cost, low power system on a chip microcontrollers with integrated Wi-Fi and dual-mode Bluetooth");
+
+addComponent("Drivers", "Arduino","3", "red","Open-source electronic prototyping platform enabling users to create interactive electronic objects");
+addComponent("Drivers", "Linkit","4","red"," development platform enables you to design and prototype Wearable and Internet of Things (IoT) devices, using hardware and an API");
+addComponent("Drivers", "RaspberrPi","6","red","tiny and affordable computer that you can use to learn programming through fun, practical projects");
 addComponent("Drivers", "Curie","10","red");
-addComponent("Drivers", "ARM","8","red");
+addComponent("Drivers", "ARM","8","red","Arm is the industry's leading supplier of microprocessor technology, offering the widest range of microprocessor cores to address the performance, power and cost requirements for almost all application");
 addComponent("Drivers", "NRF","10","red");
-addComponent("Drivers", "BeagleBone","9","red");
-addComponent("Drivers", "ESP8266","9","red");
+addComponent("Drivers", "BeagleBone","9","red","a low-power open-source single-board computer");
+addComponent("Drivers", "ESP8266","9","red","a low-cost Wi-Fi chip with full TCP/IP stack and MCU (microcontroller unit) capability");
 
-addComponent("Low Level Interfaces", "I2C","3", "blue");
-addComponent("Low Level Interfaces", "SPI","4","blue");
-addComponent("Low Level Interfaces", "1 Wire","6","blue");
-addComponent("Low Level Interfaces", "Ethernet","10" ,"blue");
-addComponent("Low Level Interfaces", "Serial","8" ,"blue");
-addComponent("Low Level Interfaces", "Analog","10" ,"blue");
-addComponent("Low Level Interfaces", "MQTT","9" ,"blue");
-addComponent("Low Level Interfaces", "FDX","9" ,"blue");
+addComponent("Low Level Interfaces", "I2C","8", "blue","A serial protocol for two-wire interface to connect devices like microcontrollers, I/O interfaces and other similar peripherals in embedded systems");
+addComponent("Low Level Interfaces", "SPI","8","blue","The Serial Peripheral Interface bus is a synchronous serial communication interface specification used for short distance communication, primarily in embedded systems");
+addComponent("Low Level Interfaces", "1 Wire","6","blue","1-Wire® technology is a serial protocol using a single data line plus ground reference for communication");
+addComponent("Low Level Interfaces", "Ethernet","10" ,"blue","network protocol that controls how data is transmitted over a LAN");
+addComponent("Low Level Interfaces", "Serial","8" ,"blue","serial communication is the process of sending data one bit at a time, sequentially, over a communication channel or computer bus");
+addComponent("Low Level Interfaces", "Analog","4" ,"blue");
+addComponent("Low Level Interfaces", "MQTT","9" ,"blue","publish-subscribe-based messaging protocol");
+addComponent("Low Level Interfaces", "FDX","9" ,"blue","Fast Data eXchange is a proprietary maritime communication protocol");
 
-addComponent("Sensors", "Camera","3", "orange");
-addComponent("Sensors", "Temperature","4","orange");
-addComponent("Sensors", "Humidity","6","orange");
-addComponent("Sensors", "Vibration","10" ,"orange");
-addComponent("Sensors", "Knock","8" ,"orange");
-addComponent("Sensors", "Infrared","10" ,"orange");
-addComponent("Sensors", "Hall","9" ,"orange");
-addComponent("Sensors", "Orientation","9" ,"orange");
-addComponent("Sensors", "Bend/Flex","9" ,"orange");
-addComponent("Sensors", "Ultrasonic","12" ,"orange");
-addComponent("Sensors", "Pressure","3" ,"orange");
-addComponent("Sensors", "Color","4" ,"orange");
-addComponent("Sensors", "Light","4" ,"orange");
+addComponent("Sensors", "Camera","3", "orange","optical instrument for recording or capturing images");
+addComponent("Sensors", "Temperature","4","orange","A temperature sensor is a device, typically, a thermocouple or RTD, that provides for temperature measurement through an electrical signal");
+addComponent("Sensors", "Humidity","6","orange","A humidity sensor (or hygrometer) senses, measures and reports the relative humidity in the air. It therefore measures both moisture and air temperature");
+addComponent("Sensors", "Vibration","10" ,"orange","for measuring, displaying, and analyzing linear velocity, displacement and proximity, or acceleration");
+addComponent("Sensors", "Knock","8" ,"orange","creates a voltage signal based on the vibrations caused by detonation");
+addComponent("Sensors", "Infrared","10" ,"orange","sense certain characteristics of its surroundings by either emitting and/or detecting infrared radiation.");
+addComponent("Sensors", "Hall","9" ,"orange","varies its output voltage in response to a magnetic field");
+addComponent("Sensors", "Orientation","9" ,"orange","gives information about position compared to a reference plane");
+addComponent("Sensors", "Bend/Flex","9" ,"orange","measures the amount of deflection or bending");
+addComponent("Sensors", "Ultrasonic","12" ,"orange","measures the distance to an object by using sound waves");
+addComponent("Sensors", "Pressure","3" ,"orange","measures the pressure of a gas or a liquid against a diaphragm made of stainless steel, silicon, etc., and converts the measured value into an electrical signal as an output");
+addComponent("Sensors", "Color","4" ,"orange","emits light and then look at the reflected light to determine an object’s color");
+addComponent("Sensors", "Light","4" ,"orange","changes its resistance when light shines on it");
 
-addComponent("Actuators", "Displays","3", "GreenYellow");
-addComponent("Actuators", "Servo","4","GreenYellow");
-addComponent("Actuators", "Relay","6","GreenYellow");
-addComponent("Actuators", "DC Motor","10" ,"GreenYellow");
-addComponent("Actuators", "Stepper Motor","8" ,"GreenYellow");
-addComponent("Actuators", "Solenoid Valves","10" ,"GreenYellow");
-addComponent("Actuators", "Fan","9" ,"GreenYellow");
-addComponent("Actuators", "LED/WS2812","8" ,"GreenYellow");
+addComponent("Actuators", "Displays","8", "GreenYellow","display are used to display visual characters, images or information");
+addComponent("Actuators", "Servo","8","GreenYellow", "a rotary actuator or linear actuator that allows for precise control of angular or linear position, velocity and acceleration");
+addComponent("Actuators", "Relay","6","GreenYellow", "an electrically operated switch");
+addComponent("Actuators", "DC Motor","10" ,"GreenYellow", "converts electrical energy (direct current system) into mechanical energy");
+addComponent("Actuators", "Stepper Motor","8" ,"GreenYellow","Stepper motors are DC motors that move in discrete steps");
+addComponent("Actuators", "Solenoid Valves","4" ,"GreenYellow","an electromechanically operated valve");
+addComponent("Actuators", "Fan","4" ,"GreenYellow","an apparatus with rotating blades that creates a current of air for cooling or ventilation");
+addComponent("Actuators", "LED/WS2812","8" ,"GreenYellow","emits visible light when an electric current passes through it");
 
 
 
